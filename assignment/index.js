dbc7120ca72120c6e31fbc904aa9edefe39b81a7
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2023-06-18 19:29:21 +0200

    q1 done

--- assignment/index.js
+++ assignment/index.js
@@ -1,9 +1,18 @@
 //Write JS code that solve the following problems
 //Q1. Using an if-else statement, determine if a number is divisible by 3, by 5, or both.
 
-let q1test = 15;
+let q1test = 4;
 
 //Add code here
+if(q1test%3 === 0 && q1test%5 === 0 ){
+    console.log(q1test + " is divisble by 3 and by 5");
+}else if(q1test%5 === 0){
+    console.log(q1test + " is divisible by 5");
+}else if(q1test%3 === 0){
+    console.log(q1test + " is divisble by 3");
+}else{
+    console.log(q1test + " is not divisble by 3 or 5");
+}
 
 //Q2. Using an if-else statement, determin is a year is a leap year or not.
 //Research what are the conditions to be considered a leap year.
