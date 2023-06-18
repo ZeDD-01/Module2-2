be490ff3c9459e2d0f8c1f0c8fe8041dd0614129
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2023-06-18 20:10:08 +0200

    q2

--- assignment/index.js
+++ assignment/index.js
@@ -17,12 +17,27 @@ if(q1test%3 === 0 && q1test%5 === 0 ){
 //Q2. Using an if-else statement, determin is a year is a leap year or not.
 //Research what are the conditions to be considered a leap year.
 
-let year = 1999;
-// let year = 2000;
-// let year = 1900;
-// let year = 2024;
+//let year = 1999;
+//let year = 2000;
+//let year = 1900;
+let year = 2024;
 
 //Add code here
+
+let yes = " is a leap year";
+let no = " is not a leap year"
+
+if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0 ){
+    console.log(year + yes);
+}else if(year%4 === 0 && year%100 === 0 && year !== 0){
+    console.log(year + no);
+}else if(year%4 === 0 && year%100 !== 0){
+    console.log(year + yes);
+}else{
+    console.log(year + no);
+}
+
+    
 
 //Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
 
